955837743a02c59c9c27c95588cad2225ae71354
yaki <kiwidancebad@gmail.com>
2022-09-19 22:45:26 +0300

    initial commit

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Ivan Gubanov on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
